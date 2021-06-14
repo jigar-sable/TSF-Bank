689731c328756f5ac31dfed0e27028889360f07a
Jigar Sable <64949957+jigar-sable@users.noreply.github.com>
2021-06-15 01:38:12 +0530

    Update userdetails.php

--- userdetails.php
+++ userdetails.php
@@ -8,8 +8,8 @@ if(isset($_POST['submit']))
     $amount = $_POST['amount'];
 
     $sql = "SELECT * from users where id=$from";
-    $query = mysqli_query($conn,$sql);
     $sql1 = mysqli_fetch_array($query); // returns array or output of user from which the amount is to be transferred.
+    $query = mysqli_query($conn,$sql);
 
     $sql = "SELECT * from users where id=$to";
     $query = mysqli_query($conn,$sql);
@@ -18,21 +18,21 @@ if(isset($_POST['submit']))
     // constraint to check input of negative value by user
     if (($amount)<0) {
         echo '<script type="text/javascript">';
-        echo ' alert("Oops! Negative values cannot be transferred")';  // showing an alert box.
+        echo ' alert("Oops! Negative values cannot be transferred!")';  // showing an alert box.
         echo '</script>';
     }
 
     // constraint to check insufficient balance.
     else if($amount > $sql1['balance']) {
         echo '<script type="text/javascript">';
-        echo ' alert("Bad Luck! Insufficient Balance")';  // showing an alert box.
+        echo ' alert("Insufficient Balance!")';  // showing an alert box.
         echo '</script>';
     }
     
     // constraint to check zero values
     else if($amount == 0){
          echo "<script type='text/javascript'>";
-         echo "alert('Oops! Zero value cannot be transferred')";
+         echo "alert('Oops! Zero value cannot be transferred!')";
          echo "</script>";
         }
         else {
